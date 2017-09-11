670a63a431411752a5d6173f3693f624dd3ce08b
tapos ghosh <tapos.aa@gmail.com>
2017-09-12 01:58:26 +0600

    work done

--- app/src/main/java/com/example/tapos/fragmentday1/FragmentA.java
+++ app/src/main/java/com/example/tapos/fragmentday1/FragmentA.java
@@ -6,16 +6,27 @@ import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 /**
  * Created by tapos on 9/11/2017.
  */
 
-public class FragmentA  extends Fragment{
+public class FragmentA  extends Fragment {
+    TextView result;
+    private int output;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_a,container,false);
+        View view = inflater.inflate(R.layout.fragment_a, container, false);
+
+        result = (TextView) view.findViewById(R.id.textResult);
+        result.setText(this.output + "");
         return view;
+    }
+
+    public void setData(int data) {
+        this.output = data;
     }
 }
--- app/src/main/java/com/example/tapos/fragmentday1/MainActivity.java
+++ app/src/main/java/com/example/tapos/fragmentday1/MainActivity.java
@@ -5,105 +5,48 @@ import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Toast;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     FragmentManager fragmentManager;
+    EditText esFirst;
+    EditText esSecond;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         fragmentManager = getFragmentManager();
+        esFirst = (EditText) findViewById(R.id.etFirstNumber);
+        esSecond = (EditText) findViewById(R.id.etSecondNumber);
 
 
     }
 
-    public void addA(View view) {
-        FragmentA fragmenta = new FragmentA();
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.add(R.id.container,fragmenta,"fragA");
-        transaction.commit();
-    }
 
-    public void removeA(View view) {
+    public void sendDataFragmentA(View view) {
 
-        FragmentA fragmentA = (FragmentA) fragmentManager.findFragmentByTag("fragA");
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-        if(fragmentA !=null) {
-            transaction.remove(fragmentA);
-            transaction.commit();
-        }else{
-            Toast.makeText(this,"Fragment A not found",Toast.LENGTH_SHORT).show();
-        }
-    }
-
-
-    public void addB(View view) {
-        FragmentB fragmentB = new FragmentB();
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.add(R.id.container,fragmentB,"fragB");
-        transaction.commit();
-    }
-
-    public void removeB(View view) {
-        FragmentB fragmentB = (FragmentB) fragmentManager.findFragmentByTag("fragB");
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-        if(fragmentB !=null) {
-            transaction.remove(fragmentB);
-            transaction.commit();
-        }else{
-            Toast.makeText(this,"Fragment B not found",Toast.LENGTH_SHORT).show();
-        }
-    }
-
-    public void replaceByFragmentA(View view) {
+        int firstNumber = Integer.parseInt(esFirst.getText().toString());
+        int secondNumber = Integer.parseInt(esFirst.getText().toString());
+        int result = firstNumber + secondNumber;
+        Bundle bundle = new Bundle();
+        bundle.putInt("resultData",result);
         FragmentA fragmentA = new FragmentA();
+        fragmentA.setArguments(bundle);
         FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.replace(R.id.container,fragmentA,"fragA");
+        transaction.add(R.id.container,fragmentA,"fragA");
         transaction.commit();
+
     }
 
-    public void replaceByFragmentB(View view) {
-        FragmentB fragmentB = new FragmentB();
+    public void sendDataFragmentAAnother(View view) {
+        int firstNumber = Integer.parseInt(esFirst.getText().toString());
+        int secondNumber = Integer.parseInt(esFirst.getText().toString());
+        int result = firstNumber + secondNumber;
+        FragmentA fragmentA = new FragmentA();
+        fragmentA.setData(result);
         FragmentTransaction transaction = fragmentManager.beginTransaction();
-        transaction.replace(R.id.container,fragmentB,"fragB");
+        transaction.add(R.id.container,fragmentA,"fragA");
         transaction.commit();
-    }
-
-    public void attachFragmentA(View view) {
-        FragmentA fragmentA = (FragmentA) fragmentManager.findFragmentByTag("fragA");
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-        if(fragmentA !=null) {
-            transaction.attach(fragmentA);
-            transaction.commit();
-        }
-    }
-
-    public void detachFragmentA(View view) {
-        FragmentA fragmentA = (FragmentA) fragmentManager.findFragmentByTag("fragA");
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-        if(fragmentA !=null) {
-            transaction.detach(fragmentA);
-            transaction.commit();
-        }
-    }
-
-    public void showFragmentA(View view) {
-        FragmentA fragmentA = (FragmentA) fragmentManager.findFragmentByTag("fragA");
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-        if(fragmentA !=null) {
-            transaction.show(fragmentA);
-            transaction.commit();
-        }
-    }
-
-    public void hideFragmentA(View view) {
-        FragmentA fragmentA = (FragmentA) fragmentManager.findFragmentByTag("fragA");
-        FragmentTransaction transaction = fragmentManager.beginTransaction();
-        if(fragmentA !=null) {
-            transaction.hide(fragmentA);
-            transaction.commit();
-        }
     }
 }
